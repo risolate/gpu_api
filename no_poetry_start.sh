592f171b14ca84118d9b7c3ad55ba00e945b8251
risolate <rjh4270@naver.com>
2023-02-06 01:30:46 +0000

    feat: log and gitignore

--- no_poetry_start.sh
+++ no_poetry_start.sh
@@ -2,4 +2,4 @@
 
 pip install -r requirements.txt
 python3 predownload.py
-gunicorn -b 0.0.0.0:30001 -k uvicorn.workers.UvicornWorker -w 3 main:app
+gunicorn -b 0.0.0.0:30001 -k uvicorn.workers.UvicornWorker --access-logfile ./gunicorn-access.log -w 3 main:app
--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 poetry shell
 poetry install
 python3 predownload.py
-gunicorn -b 0.0.0.0:30001 -k uvicorn.workers.UvicornWorker -w 3 main:app
+gunicorn -b 0.0.0.0:30001 -k uvicorn.workers.UvicornWorker --access-logfile ./gunicorn-access.log -w 3 main:app
