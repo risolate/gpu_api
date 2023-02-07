71f905a188835e99b7e25860bad02c7c8877206f
risolate <rjh4270@naver.com>
2023-02-07 05:04:17 +0000

    fix: add_middleware

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
 from model import GenerationModel, Hatespeech
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 model = GenerationModel()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/purificate")
 async def generation(hatespeech: Hatespeech):
 
-    sentence = hatespeech.dict()["sentence"]
+    sentence = hatespeech.sentence
     purificated = model.purification(sentence)
 
     return {"purificated": purificated}
