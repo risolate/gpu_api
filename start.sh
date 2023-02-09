4804c7fafaa54b40930fc5b109ab414da7a741a0
risolate <rjh4270@naver.com>
2023-02-09 01:17:17 +0000

    final commit

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-poetry shell
-poetry install
-python3 predownload.py
+
 gunicorn -b 0.0.0.0:30001 -k uvicorn.workers.UvicornWorker --access-logfile ./gunicorn-access.log -w 3 main:app
